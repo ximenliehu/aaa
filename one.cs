dc4e81b16a26ccd44544f60fa4fd69a512a1f2fe
madonglei <ximenliehu@outlook.com>
2019-06-10 19:40:41 +0800

    all commit

---
+++ one.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using DotNetCore.CAP;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Services.Test1.Controllers
+{
+    [Route("api1/[controller]")]
+    [ApiController]
+    public class TestOnesController : ControllerBase
+    {
+        private readonly ICapPublisher _capBus;
+
+        public TestOnesController(ICapPublisher capPublisher)
+        {
+            _capBus = capPublisher;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            _capBus.Publish("services.test1.show.time", DateTime.Now);
+            return new string[] { "TestOnes_value1", "TestOnes_value2" };
+        }
+
+        [HttpGet]
+        [Route("health")]
+        public ActionResult<string> Health()
+        {
+            return "Health!!!!!";
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
